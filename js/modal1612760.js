018cc9c3deba43b91b7ed277dda1adc2f3eb2064
truongginjs <truongginjs@gmail.com>
2019-05-05 17:55:16 +0700

    edit profile :D

--- js/modal1612760.js
+++ js/modal1612760.js
@@ -7,3 +7,38 @@
 //   modal.find('.modal-title').text('modal-titleEdit profile ' + recipient)
 //   modal.find('.modal-body input').val(recipient)
 // })
+$(document).ready( function() {
+    $(document).on('change', '.btn-file :file', function() {
+    var input = $(this),
+        label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+    input.trigger('fileselect', [label]);
+    });
+
+    $('.btn-file :file').on('fileselect', function(event, label) {
+        
+        var input = $(this).parents('.input-group').find(':text'),
+            log = label;
+        
+        if( input.length ) {
+            input.val(log);
+        } else {
+            if( log ) alert(log);
+        }
+    
+    });
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            
+            reader.onload = function (e) {
+                $('#img-upload').attr('src', e.target.result);
+            }
+            
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+
+    $("#imgInp").change(function(){
+        readURL(this);
+    }); 	
+});
