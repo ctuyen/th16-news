b77a33eb0e6d5b640cae37c453d429c7a5a8c3fb
truongginjs <truongginjs@gmail.com>
2019-06-07 20:32:58 +0700

    fix xong loi css

--- routes/main/categories.route.js
+++ routes/main/categories.route.js
@@ -3,16 +3,18 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("main/categories",{
-      stylePage: 'category',
-      stylePageResponsive: 'category_responsive'
-  
+  res.render("main/categories", {
+    stylePage: "category",
+    stylePageResponsive: "category_responsive"
   });
 });
 
 router.get("/:category", (req, res) => {
   var category = req.params.category;
-  res.render("main/categories");
+  res.render("main/categories", {
+    stylePage: "category",
+    stylePageResponsive: "category_responsive"
+  });
 });
 
 module.exports = router;
