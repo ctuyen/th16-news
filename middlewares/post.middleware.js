e98346c4ac4e86a0b751326804f9cbfb993c8fc5
truongginjs <truongginjs@gmail.com>
2019-06-19 01:47:48 +0700

    final 1.0?

--- middlewares/post.middleware.js
+++ middlewares/post.middleware.js
@@ -1,32 +1,31 @@
-var postModel = require('../models/posts.model')
+var postModel = require("../models/posts.model");
 var authModel = require("../models/auth.model");
 
 module.exports.checkPremium = async (req, res, next) => {
+  var post = await postModel.checkPremium(req.params.idPost);
+
+  if (post.rowCount == 0 || !post.rows[0].ispremium) {
+    next();
+  } else {
     if (!req.signedCookies.userId) {
-        res.redirect('/auth/login');
-        return;
+      res.redirect("/auth/login");
+      return;
     }
-    
-    var post = await postModel.checkPremium(req.params.idPost);
-    
-    if (post.rowCount == 0 || !post.rows[0].ispremium) {
-        next()
+
+    var user = await authModel.checkId(req.signedCookies.userId);
+    if (!user.rows[0].expirationdate) {
+      res.redirect("/request-premium");
+      return;
     } else {
-        var user = await authModel.checkId(req.signedCookies.userId);
-        if (!user.rows[0].expirationdate) {
-            res.redirect("/request-premium");
-            return;
-        }
-        else {
-            let date = new Date(user.rows[0].expirationdate);
-            let dateNow = new Date();
+      let date = new Date(user.rows[0].expirationdate);
+      let dateNow = new Date();
 
-            if (dateNow.valueOf() >= date.valueOf()) {
-                res.redirect("/request-premium");
-                return;
-            }
+      if (dateNow.valueOf() >= date.valueOf()) {
+        res.redirect("/request-premium");
+        return;
+      }
 
-            next();
-        }
+      next();
     }
-}
+  }
+};
